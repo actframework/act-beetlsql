f4ef296131b333034a67225209fea94bc6562b3c
Gelin Luo <greenlaw110@gmail.com>
2018-01-13 10:20:47 +1100

    update to act-1.6 sql-common-1.2 and beetl-2.10.4

---
+++ CHANGELOG.md
@@ -0,0 +1,24 @@
+# Act BeetlSql Change Log
+
+1.3.0
+* catch up to sql-common-1.2.0
+* update to act-1.6.x
+* update to beetlsql-2.10.4
+
+1.2.1
+* catch up to sql-common-1.1.1
+
+1.2.0
+* update to act-1.4.0
+* update to act-sql-common-1.1.0
+
+1.1.1
+* update beetlsql version
+
+1.1.0a
+* Ignore BeetlClassFinder error - a temporary workaround for TechEmpower test project
+
+1.1.0
+* catch up to act-1.1.2
+
+1.0.0 - baseline version
--- src/main/java/act/db/beetlsql/BeetlSqlTransactional.java
+++ src/main/java/act/db/beetlsql/BeetlSqlTransactional.java
@@ -1,5 +1,25 @@
 package act.db.beetlsql;
 
+/*-
+ * #%L
+ * ACT Beetlsql
+ * %%
+ * Copyright (C) 2017 - 2018 ActFramework
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
 import org.beetl.sql.core.DSTransactionManager;
 import org.osgl.exception.UnexpectedException;
 import org.osgl.mvc.annotation.After;
@@ -7,8 +27,8 @@ import org.osgl.mvc.annotation.Before;
 import org.osgl.mvc.annotation.Catch;
 import org.osgl.mvc.annotation.Finally;
 
-import javax.inject.Singleton;
 import java.sql.SQLException;
+import javax.inject.Singleton;
 
 /**
  * An injector support Transaction
@@ -16,12 +36,14 @@ import java.sql.SQLException;
 @Singleton
 public class BeetlSqlTransactional {
 
-    @Before
+    public static final int INTERCEPTOR_PRIORITY = -99;
+
+    @Before(priority = INTERCEPTOR_PRIORITY)
     public void start() {
         DSTransactionManager.start();
     }
 
-    @After
+    @After(priority = -1 * INTERCEPTOR_PRIORITY)
     public void commit() {
         try {
             DSTransactionManager.commit();
@@ -30,7 +52,7 @@ public class BeetlSqlTransactional {
         }
     }
 
-    @Catch(Exception.class)
+    @Catch(value = Exception.class, priority = INTERCEPTOR_PRIORITY)
     public void rollback() {
         try {
             DSTransactionManager.rollback();
